9f906dd2451d3da55c46cfbb861737c7eb558d83
Ahmad Irfan <66177703+ahmad1101@users.noreply.github.com>
2024-11-12 12:20:36 +0500

    Signin page added with responsiveness

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,36 @@
 
+import '../stylesheets/signin.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+
 
 const SignIn = () => {
     return (
-        <div>
+        <div className='signin-div'>
+            <div className="greet-div">
+                <h2>Welcome Back</h2>
+                <p>Enter your credentials for login</p>
+            </div>
+
+            <div className="form-div">
+                <div className='form-element'>
+                    
+                     <FontAwesomeIcon icon={faUser} className='icon'/>
+                    <input placeholder="Username"></input>
+                </div>
+
+                <div className='form-element'>
+                    <FontAwesomeIcon icon={faLock} className='icon'/>
+                    <input placeholder="Password"></input>
+                </div>
+                
+                <button>Login Now</button>
+            </div>
+
+            <div className="bottom-div">
+                <p>Don't have an account? <span><a>Sign Up</a></span></p>
+            </div>
             
         </div>
     )
