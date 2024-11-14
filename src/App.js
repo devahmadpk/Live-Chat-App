9e668bdad68cc2e8f897ec2f39355d1e1bcdd437
Ahmad Irfan <66177703+ahmad1101@users.noreply.github.com>
2024-11-14 15:12:58 +0500

    Fetching username from firebase instead of static text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 
 import './App.css';
-// import Banner from './components/Banner';
-// import Search from './components/Search';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+
 
 function App() {
 
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Define the path for each component */}
+          <Route path="/" element={<SignIn />} />       {/* Default route to SignIn */}
+          <Route path="/signup" element={<SignUp />} /> {/* Route for SignUp */}
+          <Route path="/home" element={<Home />} /> {/* Route for Home */}
+        </Routes>
+      </div>
+    </Router>
+    
   )
 }
 
