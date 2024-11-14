6d9ae58bb610b11fdc9a628b3b549b793d0c6b61
Ahmad Irfan <66177703+ahmad1101@users.noreply.github.com>
2024-11-14 15:18:50 +0500

    Layout for chat cards inside sidebar

---
+++ src/components/Chats.js
@@ -0,0 +1,13 @@
+
+
+const Chats = () => {
+    return (
+        <div className="user-chat">
+            <h3>Ahmad</h3>
+            <p>Hello this is a chat</p>
+        </div>
+    )
+
+}
+
+export default Chats;
