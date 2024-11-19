dd43183efcdd107bf5f41ac24996b5d3dabd4f32
Ahmad Irfan <66177703+ahmad1101@users.noreply.github.com>
2024-11-19 12:02:17 +0500

    Added current user using context api

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import { useContext } from 'react';
+import { AuthContext } from './contexts/AuthContext';
 
 
 function App() {
+
+  const {currentUser} = useContext(AuthContext)
 
   return (
     <Router>
