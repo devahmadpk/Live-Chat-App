29a8ecd271d2b249c5f31740649069fe679f9d5f
Ahmad Irfan <66177703+ahmad1101@users.noreply.github.com>
2024-11-19 12:06:45 +0500

    User related props added for Search and Chat components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import Banner from "../components/Banner";
 import Chat from "../components/Chat";
+import Search from "../components/Search";
 import Sidebar from "../components/Sidebar";
 import '../stylesheets/home.css';
 
 
 const Home = () => {
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const handleUserSelection = (user) => {
+        setSelectedUser(user);
+    };
     return (
         <>
             <Banner />
+            <Search onSelectUser={handleUserSelection} />
 
             <div className="chat-container">
                 <Sidebar />
-                <Chat />
+                <Chat selectedUser={selectedUser} />
             </div>
         </>
     )
