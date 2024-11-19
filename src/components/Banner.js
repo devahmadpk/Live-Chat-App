81f506db231e7e85b0cc6e31e8dbb25ae7431e7d
Ahmad Irfan <66177703+ahmad1101@users.noreply.github.com>
2024-11-19 12:03:51 +0500

    Fetching username from Firebase

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,24 +1,44 @@
-
-
 import '../stylesheets/banner.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
-import {faPlus} from '@fortawesome/free-solid-svg-icons'
 import { auth } from '../firebase/firebaseConfig'; // Import Firebase auth instance
-import { signOut } from 'firebase/auth';
+import { signOut, getAuth } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from 'react';
 
 const Banner = () => {
-
-    const signOutIcon = <FontAwesomeIcon icon={faRightFromBracket} />
-    const createNewIcon = <FontAwesomeIcon icon= {faPlus} />
-
+    const [userName, setUserName] = useState('');
+    const auth = getAuth();
+    const db = getFirestore();
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const getUserName = async () => {
+            if (auth.currentUser) {
+                try {
+                    const userDoc = doc(db, "users", auth.currentUser.uid);
+                    const userSnapshot = await getDoc(userDoc);
+                    if (userSnapshot.exists()) {
+                        setUserName(userSnapshot.data().userName);
+                    } else {
+                        console.log("No such user document!");
+                    }
+                } catch (err) {
+                    console.error("Error fetching user data:", err);
+                }
+            }
+        };
+
+        getUserName();
+    }, [auth, db]);
+
+    const signOutIcon = <FontAwesomeIcon icon={faRightFromBracket} />;
 
     // Function to handle sign out
     const handleSignOut = async () => {
         try {
-            await signOut(auth); // Sign out the user
+            await signOut(auth);
             console.log('User signed out successfully');
             navigate('/'); // Redirect to the sign-in page
         } catch (err) {
@@ -28,16 +48,12 @@ const Banner = () => {
 
     return (
         <div className='banner'>
-            <h3>Your Name</h3>
+            <h3>Welcome, {userName ? userName : 'Guest'}</h3>
             <div className='button-div'>
-                <button className="create">{createNewIcon}</button>
                 <button className="signout" onClick={handleSignOut}>{signOutIcon}</button>
             </div>
-            
-
         </div>
-    )
-
-}
+    );
+};
 
 export default Banner;
